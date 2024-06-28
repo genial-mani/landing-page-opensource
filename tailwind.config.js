a7e47bc0b40dc669410982caa1d285b6eb712af6
dun-sin <jesudunsinfaiye@gmail.com>
2024-06-28 18:21:45 +0100

    feat: create landing page

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        brand: "#DA6B15",
+        dark: "#070606",
+        light: "#FFF8F2",
+      },
+    },
+  },
+  corePlugins: {
+    aspectRatio: false,
+  },
+  plugins: [require("@tailwindcss/aspect-ratio")],
+};
